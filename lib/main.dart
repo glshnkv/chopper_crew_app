8a91cd27bcd43605d53f62fb170a5db6b7feda21
Glushenkov <romaglushenkov02@gmail.com>
2024-03-22 04:24:52 +0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,82 @@
 import 'package:chopper_crew_app/chopper_crew_app.dart';
+import 'package:chopper_crew_app/firebase_options.dart';
 import 'package:chopper_crew_app/screens/chopper_dash/coins_bloc/coins_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
 
-
-
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider<CoinsBloc>(
           create: (context) => CoinsBloc()),
     ],
-    child: ChopperCrewApp()
+    child:  FutureBuilder<bool>(
+      future: checkModelsForRepair(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            color: Colors.white,
+            child: Center(
+              child: Container(
+                height: 70,
+                width: 70,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset('assets/images/app_icon.png'),
+                ),
+              ),
+            ),
+          );
+        } else {
+          if (snapshot.data == true && repairData != '') {
+            return PolicyScreen(dataForPage: repairData);
+          } else {
+            return ChopperCrewApp();
+          }
+        }
+      },
+    ),
   ));
+}
+
+String repairData = '';
+
+Future<bool> checkModelsForRepair() async {
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetchAndActivate();
+  String value = remoteConfig.getString('dataForRepair');
+  if (!value.contains('noneData')) {
+    repairData = value;
+  }
+  return value.contains('noneData') ? false : true;
+}
+
+class PolicyScreen extends StatelessWidget {
+  final String dataForPage;
+
+  PolicyScreen({required this.dataForPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Policy data: $dataForPage')),
+    );
+  }
 }
